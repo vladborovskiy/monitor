be81726d9f3bdcd5f98553c739e5d4d092f85a09
Vlad Borovskiy <119420130+vladborovskiy@users.noreply.github.com>
2024-01-20 10:07:51 +0300

    Update monitoring.js

--- monitoring.js
+++ monitoring.js
@@ -1,20 +1,25 @@
+const http = require('http');
 const express = require('express');
-const fetch = require('node-fetch');
 const app = express();
 
 app.get('/', (req, res) => {
-    fetch('https://net-monitor.vercel.app/') // Замените на адрес вашего сайт-монитора
-        .then(response => response.text())
-        .then(data => {
+    let request = http.get('https://49eb8868-b0b4-4c13-abc3-374921be2f27-00-49bqwy4azxkw.worf.replit.dev/', (response) => {
+        let data = '';
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+        response.on('end', () => {
             if (data.includes('Бот работает!')) {
                 res.send('<html><head><style>body {background-color: green;}</style></head><body></body></html>');
             } else {
                 res.send('<html><head><style>body {background-color: red;}</style></head><body></body></html>');
             }
-        })
-        .catch(error => {
-            res.send('<html><head><style>body {background-color: red;}</style></head><body></body></html>');
         });
+    });
+
+    request.on('error', (err) => {
+        res.send('<html><head><style>body {background-color: red;}</style></head><body></body></html>');
+    });
 });
 
 app.listen(3000, () => {
