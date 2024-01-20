8954b5a32410c2a9c30116a7802b423a8a608576
Vlad Borovskiy <119420130+vladborovskiy@users.noreply.github.com>
2024-01-20 10:01:56 +0300

    Update monitoring.js

--- monitoring.js
+++ monitoring.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const app = express();
 
 app.get('/', (req, res) => {
-    fetch('https://49eb8868-b0b4-4c13-abc3-374921be2f27-00-49bqwy4azxkw.worf.replit.dev/')
+    fetch('https://google.com/')
         .then(response => response.text())
         .then(data => {
             if (data.includes('Бот работает!')) {
